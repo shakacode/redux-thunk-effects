bbf9b1053334a0db0e4b84c5a6ba29d4b6e85db3
Alexey Karasev <karasev.alexey@gmail.com>
2016-11-29 15:40:23 +0100

    removed babelrc

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export const effectsMiddlewareCreator = (effectCreators) =>
 ({ dispatch, getState }) => next => action => {
   if (typeof action === 'function') {
     const effects = {};
-    Object.keys(effectCreators).each(key => {
+    Object.keys(effectCreators).forEach(key => {
       const effectCreator = effectCreators[key];
     effects[key] = effectCreator({ dispatch, getState });
   });
